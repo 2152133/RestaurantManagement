49a080b54d00f2683922652464d4c96875606c2c
Jose Machado <2152133@my.ipleiria.com>
2018-12-04 07:49:19 +0000

    Cria o componente relativo a tabela de orders

---
+++ resources/js/components/OrdersList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>State</th>
+                  <th>Item Name</th>
+                  <th>Responsable cook</th>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(order,index) in orders" :key="order.id">
+                    <td>{{order.id}}</td>
+                    <td>{{order.state}}</td>
+                    <td>{{order.item_id}}</td>
+                    <td>{{order.responsable_cook_id}}</td>
+                    <td>{{order.start}}</td>
+                    <td>{{order.end}}</td>
+                    <td>
+                        <button type="Submit" class="btn btn-primary btn-sm" @click="editOrder(order)">Edit</button>
+                        <button type="Submit" class="btn btn-danger btn-sm" @click="deleteOrder(order, index)">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["orders"],
+    data: function() {
+        return {
+            
+        }
+    },
+    methods: {
+        editOrder(order, index){
+
+        },
+        deleteUser(user, index){
+
+        },
+        mounted() {
+            
+        }
+    }
+}
+</script>
---
+++ resources/js/vue.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+
+'use strict';
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+//const VueRouter = require('vue-router');
+Vue.use(VueRouter);
+
+const ordersListComponent = Vue.component('orders-list', require('./components/OrdersList.vue'));
+const OrdersComponent = Vue.component('orders', require('./components/orders.vue'));
+
+const routes = [
+]
+
+const router = new VueRouter({
+    routes: routes
+})
+
+const app = new Vue({
+    el: '#app',
+    router: router
+});
